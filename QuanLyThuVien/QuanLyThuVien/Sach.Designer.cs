bc95addbf5c7886923deb3b15fcc8990a75675e0
Phong <58285182+nhattao1801@users.noreply.github.com>
2019-12-07 11:23:19 +0700

    Phong

--- QuanLyThuVien/QuanLyThuVien/Sach.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/Sach.Designer.cs
@@ -28,19 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fname = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.user = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button3.Location = new System.Drawing.Point(721, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 66);
+            this.button3.TabIndex = 46;
+            this.button3.Text = "XOÁ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 238);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1238, 377);
+            this.dataGridView1.TabIndex = 45;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(721, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 65);
+            this.button2.TabIndex = 44;
+            this.button2.Text = "THÊM";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(1000, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 61);
+            this.button1.TabIndex = 43;
+            this.button1.Text = "SỬA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(25, -47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(361, 39);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Thông Tin Người Dùng";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(34, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 29);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "Tên Tác Giả";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(243, 172);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 29);
+            this.textBox1.TabIndex = 40;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label7.Location = new System.Drawing.Point(34, 172);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(170, 29);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "Nhà Xuất Bản";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label6.Location = new System.Drawing.Point(34, 97);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 29);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Tên Sách";
+            // 
+            // fname
+            // 
+            this.fname.AutoSize = true;
+            this.fname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.fname.Location = new System.Drawing.Point(34, 61);
+            this.fname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(113, 29);
+            this.fname.TabIndex = 37;
+            this.fname.Text = "Mã Sách";
+            // 
+            // email
+            // 
+            this.email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.email.Location = new System.Drawing.Point(243, 135);
+            this.email.Margin = new System.Windows.Forms.Padding(4);
+            this.email.Multiline = true;
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(333, 29);
+            this.email.TabIndex = 36;
+            // 
+            // pass
+            // 
+            this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.pass.Location = new System.Drawing.Point(243, 98);
+            this.pass.Margin = new System.Windows.Forms.Padding(4);
+            this.pass.Multiline = true;
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(333, 29);
+            this.pass.TabIndex = 35;
+            // 
+            // user
+            // 
+            this.user.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.user.Location = new System.Drawing.Point(243, 61);
+            this.user.Margin = new System.Windows.Forms.Padding(4);
+            this.user.Multiline = true;
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(333, 29);
+            this.user.TabIndex = 34;
             // 
             // Sach
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1236, 569);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
             this.Name = "Sach";
             this.Text = "Thông Tin Sách";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label fname;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.TextBox user;
     }
 }
